ecc4019faae49f180285c00615c4c5a71d49a55d
aniketkardule <82253148+aniketkardule@users.noreply.github.com>
2021-12-22 09:45:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const MongoClient = require('mongodb').MongoClient;
 
+const assert = require('assert');
+
+// Connection URL
+
+const url = 'mongodb+srv://$[username]:$[password]@$[hostlist]/$[database]?retryWrites=true';
+
+// Use connect method to connect to the Server
+
+MongoClient.connect(url, function(err, client) {
+
+  assert.equal(null, err);
+
+  client.close();
+
+});
