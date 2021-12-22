6a22fa6eb05165e2391eaad1b85b4edcd10ab459
aniketkardule <82253148+aniketkardule@users.noreply.github.com>
2021-12-22 11:10:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -43,93 +43,10 @@ app.use(function(req, res, next) {
 });
 
 
-	const l = parseInt(req.query.lim);
+	
 
-	const c = parseInt(req.query.cat);
-
-	if(s && !c){
-
-	const a = await Mobiles.find({name:{$regex:s,$options:'$i'}},{_id:1,name:1,dpimg:1}).sort({_id:-1}).limit(l);
-
-	res.send(a);
-
-	}else if(s && c){
-
-   const a = await Mobiles.find({name:{$regex:s,$options:'$i'},cat:c},{_id:1,name:1,dpimg:1}).sort({_id:-1}).limit(l);
-
-   res.send(a);
-
-	}
-
-	else{
-
-	res.send([]);
-
-	}
-
-	}catch(e){
-
-		res.send(e);
-
-	}
-
-});
-
-app.get("/product", async (req,res) =>{
-
-	try{
-
-		const n = req.query.q;
-
-		const b = req.query.brand;
-
-		const c = parseInt(req.query.cat);
-
-	 const l = parseInt(req.query.lim);
-
-if(c && b){
-
-    	const s = await Mobiles.find({cat:Cat,brand:Brand}).sort({_id:-1}).limit(l);
-
-	res.send(s);
-
-}else if(n && c && b){
-
-    const s = await Mobiles.find({name:{$regex:n,$options:'i'},cat:c,brand:b}).sort({_id:-1}).limit(l);
-
-	res.send(s);
-
-}else{
-
-				res.send([]);
-
-}
-
-	}catch(e){
-
-		res.status(500).send({ message: e.message });
-
-	}
-
-});
-
-app.get("/autocomplete/:id", async (req, res) =>{
-
-	try{
-
-	const i = req.params.id;
-
-	const m = await Mobiles.findById(i,{name:1,cat:1,slug:1,mrp:1,dpimg:1}).sort({_id:-1});
-
-	res.send(m);
-
-	}catch(e){
-
-		res.status(500).send({ message: e.message });
-
-	}
-
-});
+		
+	
 
 app.get("/", async (req,res) =>{
 
