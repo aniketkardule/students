1428a9bc83c77342eaf0c492ad883101ac45f27d
aniketkardule <82253148+aniketkardule@users.noreply.github.com>
2021-12-22 10:59:44 +0530

    Update students.js

--- model/students.js
+++ model/students.js
@@ -1,1 +1,10 @@
+mongoose = require("mongoose");
 
+const MobileData = new mongoose.Schema({
+	_id: Number,
+    name: String,
+	:Number,
+	cat:Number,
+	dpimg:String
+})
+module.exports = mongoose.model('Student', MobileData);
