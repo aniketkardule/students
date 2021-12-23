ea519094655ed94b142bbe9fa6594e4282d7f461
aniketkardule <82253148+aniketkardule@users.noreply.github.com>
2021-12-23 09:42:44 +0530

    Update students.js

--- model/students.js
+++ model/students.js
@@ -2,9 +2,9 @@ mongoose = require("mongoose");
 
 const MobileData = new mongoose.Schema({
 	_id: Number,
-    name: String,
+    name: String
 
-	cat:Number,
+
 	
 })
 module.exports = mongoose.model('Student', MobileData);
