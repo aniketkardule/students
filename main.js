ce31387c1ecf0f4f57f18bf9748a3bb8b51b64ca
aniketkardule <82253148+aniketkardule@users.noreply.github.com>
2021-12-24 10:52:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -33,10 +33,22 @@ app.use(express.json());
 app.use(express.urlencoded({"extended":true}));
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://techruins.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 	
 app.post("/", (req, res, next) => {
