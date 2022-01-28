4f497cf29535560974811a86e5935e65ee173fcf
Aniket Kardule <82253148+aniketkardule@users.noreply.github.com>
2022-01-28 13:41:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/style.css">    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Murecho:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
