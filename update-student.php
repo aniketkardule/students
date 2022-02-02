1b4881f02603febd3a50f3be8137818d5c2e6cf0
Aniket Kardule <82253148+aniketkardule@users.noreply.github.com>
2022-02-02 14:05:28 +0530

    Update update-student.php

--- update-student.php
+++ update-student.php
@@ -10,12 +10,22 @@ $result = json_decode($fetch);
 
 <!DOCTYPE html>
 <html>
-          <link rel="styles/stylesheet" href="styles/style.css" />
+          <link rel="stylesheet" href="styles/style.css" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Murecho:wght@300&display=swap" rel="stylesheet">
 <style>
-input[type=text], select {
+
+.dashboard{
+  position:absolute;
+  top:10px;
+  right:11%;
+  color:yellow;
+}
+.nav-area{
+  display:none;
+}
+input[type=text],input, select {
   width: 100%;
   padding: 12px 20px;
   margin: 8px 0;
@@ -27,39 +37,57 @@ input[type=text], select {
 
 input[type=submit] {
   width: 100%;
-  background-color: #4CAF50;
-  color: white;
+  background-color:lightblue;
+  color: ;
   padding: 14px 20px;
   margin: 8px 0;
   border: none;
   border-radius: 4px;
   cursor: pointer;
 }
-
-input[type=submit]:hover {
-  background-color: #45a049;
+form{
+  width:80%;
+  margin:0 auto;
+}
+h3{
+  margin:0 auto 10px 10%;
+}
+label{
+  padding-top:10px;
+  padding-bottom:10px;
+  text-align:center;
+  display:inline-block;
+  width:100%;
+  background:lightblue;
+  border: none;
+  border-radius: 4px;
 }
 
 .c {
+  width:75%;
+  margin:0 auto;
+  position:relative;
   border-radius: 5px;
   background-color: #f2f2f2;
   padding: 20px;
 }
-	.to-dash{
-	    position:absolute;
-		top:100px;
-		right:80px;
-		font-size:18px;
-	}
+@media (max-width:600px){
+  .c{
+    width:100%;
+  }
+}
+
+
 </style>
 <body>
 <?php include "header.php";    ?>
 
-<h3 style="margin-top:70px">Enter Student Details</h3>
-	<a class="to-dash" href="students.php">Back to dashboard</a>
+
 
 <div class="c">
-  <div>
+	<h3>Enter Student Details</h3>
+	<p class="dashboard"><a href="/students.php">Go to dashboard</a></p>
+  <form action="#">
       <label>Roll no</label>
        <input type="text" id="rn" value="<?=  $result->_id;  ?>" placeholder="Enter roll no">
     <label>Name</label>
@@ -91,7 +119,7 @@ input[type=submit]:hover {
   <label>Profile photo url</label>
    <input value="<?=  $result->purl;  ?>" id="purl" type="text" placeholder="Enter Profile">
     <input id="submit" type="submit" value="Submit">
-  </div>
+  </form>
 </div>
      <script>
 
@@ -127,5 +155,9 @@ document.getElementById('submit').addEventListener('click', function(){
             });
 
      </script>
+</body>
+</html>
+
+
 </body>
 </html>
