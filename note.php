70fb93b5e2b9c17cfe517fbd930abf019f0ad3b9
Aniket Kardule <82253148+aniketkardule@users.noreply.github.com>
2022-02-03 09:16:52 +0530

    Update note.php

--- note.php
+++ note.php
@@ -1,16 +1,50 @@
-
-<html>
-  <head>
+<html><head>
     <link rel="stylesheet" href="styles/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Murecho:wght@300&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Murecho:wght@300&amp;display=swap" rel="stylesheet">
+  <style>
+    .c{
+      width:75%;
+      margin:30px auto;
+      position:relative;
+      background-color:#f2f2f2;
+    }
+    .note{
+      width:80%;
+      background-color:#fff;
+      margin:0 auto;
+    }
+    .box{
+      padding:10px;
+      border-radius:7px;
+    }
+    h3{
+      width:80%;
+      margin:0 auto;
+    }
+    .dashboard{
+      position:absolute;
+      top:20px;
+      right:10%;
+    }
+    @media (max-width:600px){
+      .c, .note, h3{
+        width:100%;
+      }
+      .dashboard{
+        right:20px;
+      }
+    }
+  </style>
   
   </head>
   <body>
-    <div class="c">
-      <p class="note">
+    <div class="c box">
+      <h3 class="box">Note by Developer</h3>
+      <a class="dashboard" href="students.php">Go to Dashboard</a>
+      <p class="note box">
         fhbjdnks iodlsx ks ivwckde   binoxdqks bdinow bi2ndoqxk bifndoxk binodck  bifnekc  bifnkc  bifnoc 9on2clk binokc  ioneck ifnock oncl nonmek nioeck
         fhbjdnks iodlsx ks ivwckde   binoxdqks bdinow bi2ndoqxk bifndoxk binodck  bifnekc  bifnkc  bifnoc 9on2clk binokc  ioneck ifnock oncl nonmek nioeck
         fhbjdnks iodlsx ks ivwckde   binoxdqks bdinow bi2ndoqxk bifndoxk binodck  bifnekc  bifnkc  bifnoc 9on2clk binokc  ioneck ifnock oncl nonmek nioeck
@@ -23,5 +57,6 @@
       </p>
     
     </div>
-  </body>
-</html>
+  
+
+</body></html>
