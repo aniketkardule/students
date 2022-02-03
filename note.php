8341f7e382b05c3143d5a30bbe03a30c7c600f2c
Aniket Kardule <82253148+aniketkardule@users.noreply.github.com>
2022-02-03 08:51:17 +0530

    Update note.php

--- note.php
+++ note.php
@@ -3,6 +3,9 @@
   <head>
     <link rel="stylesheet" href="styles/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Murecho:wght@300&display=swap" rel="stylesheet">
   
   </head>
   <body>
