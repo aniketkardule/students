ce975af602fdb1bef86cc8e64d7910f8f6a47f25
Aniket Kardule <82253148+aniketkardule@users.noreply.github.com>
2022-02-03 08:46:50 +0530

    Update update-student.php

--- update-student.php
+++ update-student.php
@@ -11,6 +11,7 @@ $result = json_decode($fetch);
 <!DOCTYPE html>
 <html>
           <link rel="stylesheet" href="styles/style.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Murecho:wght@300&display=swap" rel="stylesheet">
