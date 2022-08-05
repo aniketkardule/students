8689da9f3e61ee425771f42910ef5f0a1d79cdbd
Aniket Kardule <82253148+aniketkardule@users.noreply.github.com>
2022-08-05 19:51:15 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,12 +1,5 @@
 <div class="wrapper">
         <div class="logo">
-                <span style="color:#fff;font-size:24px;margin:auto auto auto 21px;font-weight:bold;">Student Diary</span>
+                <span>Student Diary</span>
         </div>
-<ul class="nav-area">
-<li><a href="#">Home</a></li>
-<li><a href="#">About Us</a></li>
-<li><a href="#">Cources</a></li>
-<li><a href="#">Contact Us</a></li>
-<li><a href="students.php">Institute Login</a></li>
-</ul>
 </div>
