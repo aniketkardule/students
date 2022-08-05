9481ac02877cd5bcc5ab5e0d0c026f1c268ff58f
Aniket Kardule <82253148+aniketkardule@users.noreply.github.com>
2022-08-05 19:55:51 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div class="wrapper">
-        <div class="logo">
+        <div  style="color:#fff;font-size:24px;margin:auto;dispaly:block;font-weight:bold;" class="logo">
            Student Diary
         </div>
 </div>
